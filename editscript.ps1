1525399eb58d0653dc918c82135eb042dcf466eb
BTELNYY <74313030+BTELNYY@users.noreply.github.com>
2021-11-20 11:18:12 -0600

    Added v1.4 BETA 2

--- editscript.ps1
+++ editscript.ps1
@@ -1,6 +1,19 @@
 #YanSim Save data editor
-$version = "1.4 BETA 1"
+Add-Type -AssemblyName System.Windows.Forms
+[System.Windows.Forms.Application]::EnableVisualStyles()
+$version = "1.4 BETA 2"
 $idarray = @(1..100)
+$array = @(1,2,3,11,12,13)
+$modern = @(1,2,3)
+$old = @(11,12,13)
+$1or0 = @(0,1)
+$date = Get-Date -Format "MM-dd-yyyy"
+$seconds = Get-Date -Format "HH:mm:ss"
+$time = Get-Date
+#for later usage
+$appdata = "$env:APPDATA\BTELNYY"
+$allowdebug = $true #this will be changed later by using config, in the mean time it is just a cool little thing.
+$logfolder = "." #directory in which to put logs, I will allow modification with the registry later.
 write-host "BTELNYY's Yandere Simulator Save Game Editor v$version" -ForegroundColor "blue"
 write-host "Yandere Simulator by Alexander Stuart Mahan or YandereDev." -ForegroundColor "blue"
 write-host "Only Works for the Osana Demo or later (Including 1980's mode)" -ForegroundColor "blue"
@@ -30,10 +43,65 @@ write-host "Save Files are stored from the top being 1. and the bottom being 3 w
 write-warning "The program doesn't check if the save files are accurate, so do not lie unless you like erros."
 write-host "What Porfile is the current player on? (1-3 Ayano, 11-13 Ryoba)"
 [int32]$savegame = read-host "Value Between 1-3, 11-13"
-$array = @(1,2,3,11,12,13)
-$modern = @(1,2,3)
-$old = @(11,12,13)
-$1or0 = @(0,1)
+function log(){
+    param ([string]$text, [switch]$debug)
+    if($debug -eq $false){
+        Add-Content -Path ".\$date.log" -Value "[$seconds]: $text"
+    }elseif($debug -eq $true -and $allowdebug -eq $true){
+        Add-Content -Path "$logfolder\$date.log" -Value "[$seconds]:[DEBUG]: $text"
+    }
+}
+function errorgui(){
+    #currenlty not used, will be used when I make a GUI.
+    param([string]$msg, [string]$details)
+    log("Error occured, error msg: $msg, error details: $details")
+    $Form                            = New-Object system.Windows.Forms.Form
+    $Form.ClientSize                 = New-Object System.Drawing.Point(500,300)
+    $Form.text                       = "Error"
+    $Form.TopMost                    = $false
+    $Form.FormBorderStyle            = 'Fixed3D'
+    $Form.maximizeBox                = $false
+
+    $Panel1                          = New-Object system.Windows.Forms.TextBox
+    $Panel1.ReadOnly                 = $true
+    $Panel1.height                   = 190
+    $Panel1.width                    = 460
+    $Panel1.location                 = New-Object System.Drawing.Point(20,75)
+    $Panel1.MultiLine                = $true
+    $Panel1.WordWrap                 = $true
+    $Panel1.Scrollbars               = "Vertical"
+    $Panel1.Text                     = $details
+
+    $closebutton                     = New-Object system.Windows.Forms.Button
+    $closebutton.text                = "Close"
+    $closebutton.width               = 60
+    $closebutton.height              = 30
+    $closebutton.location            = New-Object System.Drawing.Point(440,270)
+    $closebutton.Font                = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+    $closebutton.Add_Click{
+        $Form.Close()
+        return
+    }
+
+    $Label1                          = New-Object system.Windows.Forms.Label
+    $Label1.Text                     = $msg
+    $Label1.AutoSize                 = $true
+    $Label1.width                    = 25
+    $Label1.height                   = 10
+    $Label1.location                 = New-Object System.Drawing.Point(5,15)
+    $Label1.Font                     = New-Object System.Drawing.Font('Microsoft Sans Serif',10)
+
+    $Form.controls.AddRange(@($closebutton,$Panel1,$TextBox1,$TextBox2,$Label1,$Button1,$CheckBox1,$closebutton2,$checkbox2,$TextBox5,$GroupLabel,$Button3,$Button4))
+    [void]$Form.ShowDialog()
+}
+#errorgui -msg "error message" -details "error details"
+#keep above line for later
+Add-Content -Path ".\$date.log" -Value "Starting Log, it is currently $time"
+if((Test-Path -Path "$env:APPDATA\btelnyy") -eq $false){
+    New-Item -path "$env:APPDATA\" -Name "btelnyy" -ItemType "directory" | Out-Null
+}else{
+    log("ignored creation of appdata directory as it already exists.")
+}
 while($true){
     if($savegame -notin $array){
         Write-host "ERROR: invalid save game specified." -ForegroundColor "red" 
@@ -50,9 +118,15 @@ while($true){
         write-host "ERROR: I have no idea how you got here." -ForegroundColor "red"
     }
 }      
+log("The current profile is $savegame")
+log("The current registry path is $regpath")
+log("The current appdata directory is $appdata")
 #disabled until I can figure out how to edit JSON files.
 #$gamepath = read-host "Enter the games directory (Where EXE is located)"
 #write-host "Warning: Pointing to a invalid folder won't trigger any errors, just please don't" -ForegroundColor "yellow"
+
+
+
 function cmdhelp{
     write-host "Help: `n Commands: `n  help: Displays this message. `n  npc: change npc stats and such. (no, not the json ones, maybe later) `n  config: change game wide configuration. `n  exit: stops the utility. `n  tinker: opens the tinker menu. `n  deletesave: deletes save data. `nKeep in mind that everything is CaSe SeNsItIvE"
 }
@@ -76,6 +150,7 @@ function cmdconfig{
                 write-host "Error: Value must be 0 or 1." -ForegroundColor "red"
             }else{
                 Set-ItemProperty -Path "$regpath" -Name "$debug" -Value $debugvalue
+                log("debug key for current profile is $debug")
                 Get-ItemProperty -Path "$regpath" -Name $debug
                 write-host "Done." -ForegroundColor "green"
                 if($profiletype -eq 0){
@@ -169,15 +244,16 @@ function cmdconfig{
     return
 }
 function cmdnpc{
+    log("Entered NPC submenu")
     while($true){
-        write-host "NPC: help, rep, state, kidnap, exit."
+        write-host "NPC: help, rep, state, kidnap, photo, exit."
         $promptnpc = read-host "COMMAND"
-        $arraynpc = @('help','exit','state','rep','kidnap')
+        $arraynpc = @('help','exit','state','rep','kidnap','photo')
         if($promptnpc -notin $arraynpc){
             write-host "ERROR: $promptnpc isn't a command."
         }
         elseif($promptnpc -eq "help"){
-            write-host "Help for NPC commands: `n  help: displays this message `n  exit: goes to main menu `n  state: sets the 'isDying', not sure if this kills them. `n  rep: changes a characters repuation. `n  revive: allows the player to bring back dead NPC's. `n  kidnap: allows the player to manage victims and switch them."
+            write-host "Help for NPC commands: `n  help: displays this message `n  exit: goes to main menu `n  state: sets living status for NPCs. `n  rep: changes a characters repuation. `n  kidnap: allows the player to manage victims and switch them. `n photo: allows players to have 'photos' of students. (ID Screen)."
         }
         elseif($promptnpc -eq "rep"){
             if($profiletype -eq "1"){
@@ -193,10 +269,10 @@ function cmdnpc{
                 write-host "Enter a reputation value, must be a integer, can be negative"
                 $rep = read-host -Prompt "VALUE"
                 $npc = "Profile_" + "$savegame" + "_StudentReputation_" + "$npcid"
-                write-host $npc
                 $npcrep = ($regvalues | Select-String -Pattern "$npc")
                 Set-ItemProperty -Path "$regpath" -Name "$npcrep" -Value $rep
                 Get-ItemProperty -Path "$regpath" -Name "$npcrep"
+                log("Changed NPC $npcid's reputation to $rep with key $npc")
                 write-host "Done." -ForegroundColor "green"
             }
             #TO DO: check if the value is int32, but powershell refuses to let me do it.
@@ -211,6 +287,7 @@ function cmdnpc{
             #}
         }
         elseif($promptnpc -eq "exit"){
+            log("Exit NPC submenu")
             break
         }
         #elseif($promptnpc -eq ""){
@@ -293,7 +370,7 @@ function cmdnpc{
                     if($releaseprompt -notin $idarray){
                         write-host "ERROR: ID is invalid." -ForegroundColor "red"
                     }else{
-                        $temp = "Profile_" + "$savegame" + "_StudentKidnapped_" + "$releaseprompt"
+                        $temp = "Profile_" + "$savegame" + "_StudentKidnapped_" + "$releaseprompt" + "_"
                         $kidnapvar2 = ($regvalues | Select-String -Pattern "$temp") #kill me later.
                         write-host "Enter a state for ID $releaseprompt, 1 being kidnapped, 0 being at school."
                         $releasevalue = read-host "VALUE"
@@ -307,10 +384,71 @@ function cmdnpc{
                     }
                 }
             }
+        }elseif($promptnpc -eq "photo"){
+            log("Photo prompt ready.")
+            write-host "Enter a NPC ID to change status of Photograph."
+            $npcid = read-host "ID"
+
+            if($npcid -notin $idarray){
+                write-host "ERROR: Invalid ID specified." -ForegroundColor "red"
+                log("User specified invalid ID for the photo prompt.")
+            }else{
+                $newphoto = "Profile_$savegame" + "_StudentPhotographed_$npcid"
+                $oldphoto = "Profile_$savegame" + "_StudentPhotographed_" + "$npcid" + "_h"
+                $oldphoto2 = $regvalues | Select-String -Pattern "$oldphoto"
+                write-host $oldphoto2
+                write-host $temp2
+                write-host "Enter value: 1 being photographed (true) and 0 being not photographed (false)"
+                [int32]$value = read-host -Prompt "VALUE" 
+                $verify = (Get-Item $regpath).Property -contains $newphoto
+                $verify2 = (Get-ITem $regpath).Property -contains $oldphoto2
+                if($verify -eq $true){
+                    $temp = "Profile_$savegame" + "_StudentPhotographed_$npcid"
+                    Set-ItemProperty -path $regpath -name $temp -value $value
+                    write-host "Done." -ForegroundColor "green"
+                    log("User has not reset the game to title screen, $temp in $regpath has been set to $value")
+                }elseif($verify -eq $false -and $verify2 -eq $false){
+                    log("[DEBUG] value 'verify' is $verify and value 'verify2' is $verify2")
+                    $temp = "Profile_$savegame" + "_StudentPhotographed_$npcid"
+                    New-ItemProperty -path $regpath -Name $temp -Value 0
+                    Set-ItemProperty -path $regpath -Name $temp -Value $value
+                    write-host "Done." -ForegroundColor "green"
+                    log("Wrote new value called $temp in $regpath with a value of $value")
+                }else{
+                    log("Changed value in $regpath with name $oldphoto2 to value $value")
+                    $temp = "Profile_$savegame" + "_StudentPhotographed_" + "$npcid" + "_h"
+                    $temp2 = $regvalues | Select-String -Pattern $temp
+                    Set-ItemProperty -Path $regpath -Name $temp2 -Value $value
+                    write-host "Done." -ForegroundColor "green"
+                }
+                       #disabled until I need it
+                <#if($temp2 -eq $null){ #the things I need to do.
+                    log("Wrote new value called $temp in $regpath with a value of $value")
+                    New-ItemProperty -Path $regpath -Name $temp -Value 0 -Force
+                    Set-ItemProperty -path $regpath -Name $temp -Value $value
+                    write-host "Since this person wasn't photographed before, a new value was written."
+                    write-host "Done." -ForegroundColor "green"
+                }elseif($verify -ne $null){
+                    $temp = "Profile_$savegame" + "_StudentPhotographed_" + "$npcid" + "_"
+                    $temp2 = $regvalues | Select-String -Pattern $temp
+                    log("modified value $temp2 in $regpath with a new value of $value")
+                    Set-ItemProperty -path $regpath -Name $temp2 -Value $value
+                    Get-ItemProperty -path $regpath -Name $temp2
+                    write-host "Done." -ForegroundColor "green"
+                }else{
+                    log("Wrote new value called $temp in $regpath with a value of $value")
+                    New-ItemProperty -Path $regpath -Name $temp -Value 0 -Force
+                    Set-ItemProperty -path $regpath -Name $temp -Value $value
+                    write-host "Since this person wasn't photographed before, a new value was written."
+                    write-host "Done." -ForegroundColor "green"
+                }#>
+                
+            } 
         }
     }
 }
 function tinker{
+    log("Entered Tinker Meno")
     $regpath = "HKCU:\Software\YandereDev\YandereSimulator\"
     $regvalues = Get-Item -Path $regpath | Select-Object -ExpandProperty Property
     $tinkercmd = @('list','search','modify','exit','read','exists')
@@ -324,6 +462,7 @@ function tinker{
         if($tinkerinput -notin $tinkercmd){
             write-host "ERROR: $tinkerinput isn't a command." -ForegroundColor "red"
         }elseif($tinkerinput -eq "exit"){
+            log("Exited Tinker Menu")
             break
         }elseif($tinkerinput -eq "list"){
             Get-Item -Path $regpath | Select-Object -ExpandProperty Property
@@ -382,17 +521,21 @@ while($true){
             tinker
         }
         elseif($prompt -eq "exit"){
+            log("Shutting Down Application at $time")
             write-host "Goodbye."
             exit
         }
         elseif($prompt -eq "deletesave"){
+            log("entered delete save submenu")
             write-host "DANGER! this will destroy all config related to Yandere Simulator, Save files, Config, etc. You will have to run the game again to use this tool." -ForegroundColor "cyan"
             $deleteall = read-host "Delete all data? [y/n]"
             if($deleteall -eq "y"){
                 Remove-Item -Path $regpath -Force
                 write-host "All data deleted."
+                log("All save data was destroyed by deletesave.")
             }else{
                 write-host "Aborted."
+                log("exited delete save submenu (Aborted)")
             }
         }
     }
