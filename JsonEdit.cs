a3208b9957d7d8040e3be34b384f327a720ba189
loaflover <34983803+loaflover@users.noreply.github.com>
2021-11-26 00:11:46 +0200

    Update JsonEdit.cs

--- JsonEdit.cs
+++ JsonEdit.cs
@@ -8,5 +8,19 @@ namespace YanSimSaveEditor
 {
     public class JSONEdit
     {
+        public static string Delete(string JSONPath, string name)
+        {
+            //creates a key
+            try
+            {
+                
+                return "succeeded";
+            }
+            catch (Exception e)
+            {
+                //returns error as string
+                return e.ToString();
+            }
+        }
     }
 }
