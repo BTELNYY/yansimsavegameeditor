c4502da7286c96603f058542cbe4fd0bef0c2902
BTELNYY <74313030+BTELNYY@users.noreply.github.com>
2021-11-25 18:31:33 -0600

    Push

--- Program.cs
+++ Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Runtime.Remoting.Messaging;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Microsoft.Win32;
 
 namespace YanSimSaveEditor
 {
@@ -21,7 +22,32 @@ namespace YanSimSaveEditor
             //check if the games directory is set and exists, if not, call set game dir. (open)
             Open SetGameDir = new Open();
             //logic stuff
+            RegistryKey config = Registry.CurrentUser.CreateSubKey("SOFTWARE\\btelnyy\\YanSaveEdit");
+            try
+            {
+                try
+                {
+                    string result = config.GetValue("gamePath").ToString();
+                }
+                catch(Exception e)
+                {
+                    string result = SetGameDir.ShowDialog().ToString();
+                    Utility.WriteInfo(result, "Result");
+                    config.SetValue("gamePath", result);
+                };
 
+                config.Close();
+                //if(result.Length < 0)
+                //{
+                  //  SetGameDir.ShowDialog();
+                //}
+            }
+            catch (Exception e)
+            {
+                //returns error as string
+                Utility.WriteError(e.ToString(), "Error While opening registry key");
+            }
+            
 
             Application.Run(new MainForm());
             //Dont put anything Bellow this statment or it will never run.
