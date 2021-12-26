090b29b21c02f44cfa36e9b700de08b0649813bc
BTELNYY <74313030+BTELNYY@users.noreply.github.com>
2021-12-26 01:22:26 -0600

    writing log func

---
+++ YanSaveEditor/LoggerScript.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YanSimSaveEditor
+{
+    public class Log
+    {
+        //ill add more tomorrow, too tired rn.
+    }
+}
