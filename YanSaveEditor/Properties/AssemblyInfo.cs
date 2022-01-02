61ca2274c054869024754684632c496be5be26a9
BTELNYY <74313030+BTELNYY@users.noreply.github.com>
2022-01-01 18:35:45 -0600

    release v1.6.4

--- YanSaveEditor/Properties/AssemblyInfo.cs
+++ YanSaveEditor/Properties/AssemblyInfo.cs
@@ -10,7 +10,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("btelnyy")]
 [assembly: AssemblyProduct("YanSimSaveEditor")]
-[assembly: AssemblyCopyright("Copyright ©  2021")]
+[assembly: AssemblyCopyright("Copyright ©  btelnyy & loaflover 2022")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.6.3")]
-[assembly: AssemblyFileVersion("1.6.3")]
+[assembly: AssemblyVersion("1.6.4")]
+[assembly: AssemblyFileVersion("1.6.4")]
