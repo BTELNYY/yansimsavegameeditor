541114f8b61f078eb41ed0c8fa645a281175f427
BTELNYY <74313030+BTELNYY@users.noreply.github.com>
2022-03-20 11:14:09 -0600

    Release v1.8.3

--- YandereSaveEditor/Program.cs
+++ YandereSaveEditor/Program.cs
@@ -10,7 +10,7 @@ namespace YandereSaveEditor
     internal static class Program
     {
         //DO NOT FORGET TO UPDATE!!!!!!
-        public static readonly string version = "1.8.2";
+        public static readonly string version = "1.8.3";
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
